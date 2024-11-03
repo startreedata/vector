e1194caff7ba847572c4025d9335470cd3e891c9
st-omarkhalid <omar.khalid@startree.ai>
2024-11-03 16:41:20 -0600

    nit

--- src/app.rs
+++ src/app.rs
@@ -545,7 +545,6 @@ pub fn init_logging(color: bool, format: LogFormat, log_level: &str, rate: u64) 
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use warp::Reply;
-use warp::reject::sealed::IsReject;
 
 pub async fn handle_get_heap() -> Result<impl IntoResponse, (StatusCode, String)> {
     let mut prof_ctl = jemalloc_pprof::PROF_CTL.as_ref().unwrap().lock().await;
