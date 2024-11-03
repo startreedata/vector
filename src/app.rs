1608ef5d852849444805b78666c14d172c6874cc
st-omarkhalid <omar.khalid@startree.ai>
2024-11-03 16:45:09 -0600

    nit

--- src/app.rs
+++ src/app.rs
@@ -32,6 +32,13 @@ use tokio::runtime::Handle;
 
 static WORKER_THREADS: AtomicUsize = AtomicUsize::new(0);
 
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+#[allow(non_upper_case_globals)]
+#[export_name = "malloc_conf"]
+pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
 
 pub fn worker_threads() -> Option<NonZeroUsize> {
     NonZeroUsize::new(WORKER_THREADS.load(Ordering::Relaxed))
