404b7433bf13548d748182694d92435b8d509b74
st-omarkhalid <omar.khalid@startree.ai>
2024-11-03 21:20:08 -0600

    update exporter

--- src/sinks/prometheus/exporter.rs
+++ src/sinks/prometheus/exporter.rs
@@ -466,15 +466,15 @@ impl PrometheusExporter {
         tokio::spawn(async move {
             info!(message = "Building endpoint for pprofile.");
             // build our application with a route
-            let app = Router::new()
+            let app = aux::Router::new()
                 // `GET /` goes to `root`
                 .route("/debug/pprof/heap", axum::routing::get(handle_get_heap));
 
             // run our app with hyper
             // `axum::Server` is a re-export of `hyper::Server`
-            let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+            let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
             tracing::debug!("listening on {}", addr);
-            axum::Server::bind(&addr)
+            hyper::Server::bind(&addr)
                 .serve(app.into_make_service())
                 .await
                 .unwrap();
