d3662dc1685188a63648428a882309c234a07ba7
st-omarkhalid <omar.khalid@startree.ai>
2024-11-03 19:02:14 -0600

    update exporter.rs

--- src/sinks/prometheus/exporter.rs
+++ src/sinks/prometheus/exporter.rs
@@ -464,6 +464,18 @@ impl PrometheusExporter {
         let listener = tls.bind(&address).await?;
 
         tokio::spawn(async move {
+            info!(message = "Building endpoint for pprofile.");
+            let app = axum::Router::new()
+                    .route("/debug/pprof/heap", axum::routing::get(handle_get_heap));
+
+            // run our app with hyper, listening globally on port 3000
+            let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+            axum::serve(listener, app).await.unwrap();
+
+            Ok::<(), ()>(())
+        });
+
+        tokio::spawn(async move {
             info!(message = "Building HTTP server.", address = %address);
 
             Server::builder(hyper::server::accept::from_stream(listener.accept_stream()))
@@ -1660,5 +1672,26 @@ mod integration_tests {
 
         drop(tx);
         sink_handle.await.unwrap();
+    }
+}
+
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+
+pub async fn handle_get_heap() -> Result<impl IntoResponse, (StatusCode, String)> {
+    let mut prof_ctl = jemalloc_pprof::PROF_CTL.as_ref().unwrap().lock().await;
+    require_profiling_activated(&prof_ctl)?;
+    let pprof = prof_ctl
+        .dump_pprof()
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
+    Ok(pprof)
+}
+
+/// Checks whether jemalloc profiling is activated an returns an error response if not.
+fn require_profiling_activated(prof_ctl: &jemalloc_pprof::JemallocProfCtl) -> Result<(), (StatusCode, String)> {
+    if prof_ctl.activated() {
+        Ok(())
+    } else {
+        Err((axum::http::StatusCode::FORBIDDEN, "heap profiling not activated".into()))
     }
 }
