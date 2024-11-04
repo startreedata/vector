9462c0969acfafe1322fc20e4dfad3b2c2f84a11
st-omarkhalid <omar.khalid@startree.ai>
2024-11-03 18:02:03 -0600

    nit

--- src/app.rs
+++ src/app.rs
@@ -157,8 +157,8 @@ impl Application {
 
 
 // ######
-    let app = axum::Router::new()
-        .route("/debug/pprof/heap", axum::routing::get(handle_get_heap));
+//     let app = axum::Router::new()
+//         .route("/debug/pprof/heap", axum::routing::get(handle_get_heap));
 
     // run our app with hyper, listening globally on port 3000
     //let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -545,21 +545,21 @@ pub fn init_logging(color: bool, format: LogFormat, log_level: &str, rate: u64) 
 //use axum::response::IntoResponse;
 //use http::{StatusCode};
 
-pub async fn handle_get_heap() -> Result<impl axum::response::IntoResponse, (axum::http::StatusCode, String)> {
-    let mut prof_ctl = jemalloc_pprof::PROF_CTL.as_ref().unwrap().lock().await;
-    require_profiling_activated(&prof_ctl)?;
-    let pprof = prof_ctl
-        .dump_pprof()
-        .map_err(|err| (axum::http::StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
-    Ok(pprof)
-}
-
-/// Checks whether jemalloc profiling is activated an returns an error response if not.
-fn require_profiling_activated(prof_ctl: &jemalloc_pprof::JemallocProfCtl) -> Result<(), (axum::http::StatusCode, String)> {
-    if prof_ctl.activated() {
-        Ok(())
-    } else {
-        Err((axum::http::StatusCode::FORBIDDEN, "heap profiling not activated".into()))
-    }
-}
+// pub async fn handle_get_heap() -> Result<impl axum::response::IntoResponse, (axum::http::StatusCode, String)> {
+//     let mut prof_ctl = jemalloc_pprof::PROF_CTL.as_ref().unwrap().lock().await;
+//     require_profiling_activated(&prof_ctl)?;
+//     let pprof = prof_ctl
+//         .dump_pprof()
+//         .map_err(|err| (axum::http::StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
+//     Ok(pprof)
+// }
+//
+// /// Checks whether jemalloc profiling is activated an returns an error response if not.
+// fn require_profiling_activated(prof_ctl: &jemalloc_pprof::JemallocProfCtl) -> Result<(), (axum::http::StatusCode, String)> {
+//     if prof_ctl.activated() {
+//         Ok(())
+//     } else {
+//         Err((axum::http::StatusCode::FORBIDDEN, "heap profiling not activated".into()))
+//     }
+// }
 //
