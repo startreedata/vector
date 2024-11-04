92b8b7dc0f0c1e7f46b1a6955c50e7d1eccf89e0
st-omarkhalid <omar.khalid@startree.ai>
2024-11-04 15:15:13 -0600

    update app

--- src/app.rs
+++ src/app.rs
@@ -156,15 +156,6 @@ impl Application {
             Self::prepare_start(extra_context).unwrap_or_else(|code| std::process::exit(code));
 
 
-// ######
-//         let app2 = axum::Router::new()
-//             .route("/debug/pprof/heap", axum::routing::get(handle_get_heap));
-//
-//         // run our app with hyper, listening globally on port 3000
-//         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-//         serve(listener, app2).await.unwrap();
-// ######
-
         runtime.block_on(app.run())
     }
 
@@ -539,26 +530,3 @@ pub fn init_logging(color: bool, format: LogFormat, log_level: &str, rate: u64) 
     );
     info!(message = "Log level is enabled.", level = ?level);
 }
-
-//
-//use axum::http::StatusCode;
-//use axum::response::IntoResponse;
-
-// pub async fn handle_get_heap() -> Result<impl axum::response::IntoResponse, (axum::http::StatusCode, String)> {
-//     let mut prof_ctl = jemalloc_pprof::PROF_CTL.as_ref().unwrap().lock().await;
-//     require_profiling_activated(&prof_ctl)?;
-//     let pprof = prof_ctl
-//         .dump_pprof()
-//         .map_err(|err| (axum::http::StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
-//     Ok(pprof)
-// }
-//
-// /// Checks whether jemalloc profiling is activated an returns an error response if not.
-// fn require_profiling_activated(prof_ctl: &jemalloc_pprof::JemallocProfCtl) -> Result<(), (axum::http::StatusCode, String)> {
-//     if prof_ctl.activated() {
-//         Ok(())
-//     } else {
-//         Err((axum::http::StatusCode::FORBIDDEN, "heap profiling not activated".into()))
-//     }
-// }
-//
